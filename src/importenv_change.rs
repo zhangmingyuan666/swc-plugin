360a7d7e77cba9341b791822d99e95c3d7dad6dc
dinnerzhang <dinnerzhang@tencent.com>
2024-08-06 22:58:09 +0800

    fix: test

---
+++ src/importenv_change.rs
@@ -0,0 +1,82 @@
+use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
+use swc_core::{
+    common::DUMMY_SP,
+    ecma::{
+        ast::{Expr, Ident, MemberExpr, MetaPropKind, Program},
+        transforms::testing::test,
+        visit::{as_folder, FoldWith, VisitMut, VisitMutWith},
+    },
+};
+
+pub struct TransformVisitor;
+
+impl VisitMut for TransformVisitor {
+    fn visit_mut_expr(&mut self, n: &mut Expr) {
+        n.visit_mut_children_with(self);
+
+        if !n.is_member() {
+            return;
+        }
+
+        let member = n.as_mut_member().unwrap();
+        if is_visiting_import_meta_env(member).is_some() {
+            let obj = Box::new(Ident::new_no_ctxt("process".into(), DUMMY_SP).into());
+            member.obj = obj;
+        }
+    }
+}
+
+fn is_visiting_import_meta_env(n: &MemberExpr) -> Option<()> {
+    let obj = n.obj.as_meta_prop()?;
+    let prop = n.prop.as_ident()?;
+
+    if obj.kind != MetaPropKind::ImportMeta {
+        return None;
+    }
+
+    if prop.sym != "env" {
+        return None;
+    }
+
+    Some(())
+}
+
+#[plugin_transform]
+pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    program.fold_with(&mut as_folder(TransformVisitor))
+}
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    transform_import_meta_env,
+    r#"import.meta.env"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    transform_import_meta_env_prop,
+    r#"import.meta.env.MODE"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    transform_import_meta_env_key,
+    r#"import.meta.env["PROP"]"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    no_transform_import_meta,
+    r#"import.meta"#
+);
+
+test!(
+    Default::default(),
+    |_| as_folder(TransformVisitor),
+    no_transform_import_meta_glob,
+    r#"import.meta.glob"#
+);
--- src/lib.rs
+++ src/lib.rs
@@ -3,63 +3,92 @@ use swc_core::{common::DUMMY_SP, ecma::{
     transforms::testing::test_inline,
     visit::{as_folder, FoldWith, VisitMut, VisitMutWith},
 }};
+use swc_core::ecma::atoms::JsWord;
 use swc_core::plugin::{plugin_transform, proxies::TransformPluginProgramMetadata};
-
-use swc_ecma_utils::{quote_ident};
-
+use swc_core::common::SyntaxContext;
 pub struct TransformVisitor;
 
-const CONSOLE: &str = "console";
-const DEBUG: &str = "debug";
-const LOG: &str = "log";
-const SIU: &str = "siu";
-
 impl VisitMut for TransformVisitor {
-    fn visit_mut_call_expr(&mut self, call_expr: &mut CallExpr) {
-        call_expr.visit_mut_children_with(self);
+    // Implement necessary visit_mut_* methods for actual custom transform.
+    // A comprehensive list of possible visitor methods can be found here:
+    // https://rustdoc.swc.rs/swc_ecma_visit/trait.VisitMut.html
+    fn visit_mut_var_declarator(&mut self, e: &mut VarDeclarator) {
+        e.visit_mut_children_with(self);
 
-        if let Callee::Expr(callee) = &mut call_expr.callee {
-            if let Expr::Member(member) = &**callee {
-                if let (Expr::Ident(obj), MemberProp::Ident(prop)) = (&*member.obj, &member.prop) {
-                    if(&obj.sym == CONSOLE && &prop.sym == LOG) {
-                        *callee = Box::new(Expr::Member((MemberExpr {
-                            span: DUMMY_SP,
-                            obj: member.obj.to_owned(),
-                            prop: MemberProp::Ident(quote_ident!(SIU))
-                        })))
-                    }
+        let mut should_wrap = Some(false);
+        
+        if let Some(Expr::Call(CallExpr {
+            callee: Callee::Expr(callee),
+            ..
+        })) = e.init.as_deref().as_mut()
+        {
+            if let Expr::Ident(ident) = &**callee {
+                // 如果发现是此函数，要给
+                if ident.sym == *"s1sAsyncImport" {
+                    should_wrap = Some(true)
+                    /* 
+                    *callee = Box::new(Expr::Arrow((ArrowExpr {
+                        span: DUMMY_SP,
+                        body: Box::new(BlockStmtOrExpr::Expr(Default::default())),
+                        is_async: false,
+                        params: vec![],
+                        is_generator: false,
+                        return_type: None,
+                        type_params: None,
+                        ctxt: SyntaxContext::empty(),
+                    })))
+                    */
                 }
+            }
+        }
+
+        match should_wrap {
+            // 应该进行处理
+            Some(true) => {
+
+            }
+
+            // 无需进行处理
+            Some(false) => {
+
+            }
+
+            _ => {
+                
             }
         }
     }
 }
 
-test_inline!(
-    Default::default(),
-    |_| as_folder(TransformVisitor),
-    debug_stays_debug,
-    // Input codes
-    r#"console.debug("hello, world");"#,
-    // Output codes after transformed with plugin
-    r#"console.debug("hello, world");"#
-);
+/// An example plugin function with macro support.
+/// `plugin_transform` macro interop pointers into deserialized structs, as well
+/// as returning ptr back to host.
+///
+/// It is possible to opt out from macro by writing transform fn manually
+/// if plugin need to handle low-level ptr directly via
+/// `__transform_plugin_process_impl(
+///     ast_ptr: *const u8, ast_ptr_len: i32,
+///     unresolved_mark: u32, should_enable_comments_proxy: i32) ->
+///     i32 /*  0 for success, fail otherwise.
+///             Note this is only for internal pointer interop result,
+///             not actual transform result */`
+///
+/// This requires manual handling of serialization / deserialization from ptrs.
+/// Refer swc_plugin_macro to see how does it work internally.
+#[plugin_transform]
+pub fn process_transform(program: Program, _metadata: TransformPluginProgramMetadata) -> Program {
+    program.fold_with(&mut as_folder(TransformVisitor))
+}
 
+// An example to test plugin transform.
+// Recommended strategy to test plugin's transform is verify
+// the Visitor's behavior, instead of trying to run `process_transform` with mocks
+// unless explicitly required to do so.
 test_inline!(
     Default::default(),
     |_| as_folder(TransformVisitor),
-    not_interested_in_args,
+    simple_transform_global_var,
     // Input codes
-    r#"console.debug("log");"#,
-    // Output codes after transformed with plugin
-    r#"console.debug("log");"#
-);
-
-test_inline!(
-    Default::default(),
-    |_| as_folder(TransformVisitor),
-    log_to_debug,
-    // Input codes
-    r#"console.log("hello, world");"#,
-    // Output codes after transformed with plugin
-    r#"console.debug("hello, world");"#
+    r#"let isDev = s1sAsyncImport(1);"#,
+    r#"let isDev = false;"#
 );
